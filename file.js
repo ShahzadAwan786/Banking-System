3d63a92897539f995457b97c695670b32557db26
Shehzad Ashraf <stewardmax5331@gmail.com>
2025-08-05 11:31:38 +0500

    Change full codes

--- file.js
+++ file.js
@@ -1,48 +1,144 @@
-// const users = [
-//   {
-//     id: 1,
-//     account: 1,
-//     username: "Shahzad Ashraf",
-//     balance: 0,
-//     deposit: 0,
-//     withdraw: 0,
-//   },
-//   {
-//     id: 2,
-//     account: 2,
-//     username: "Wasif Ali",
-//     balance: 2000,
-//     deposit: 0,
-//     withdraw: 0,
-//   },
-//   {
-//     id: 3,
-//     account: 3,
-//     username: "Ahmad",
-//     balance: 200,
-//     deposit: 0,
-//     withdraw: 0,
-//   },
-// ];
+// const previous = localStorage.getItem("users");
+// const users = previous ? JSON.parse(previous) : [];
+// console.log(users);
+// function adduser() {
+//   const username = prompt("Add your username")?.trim();
+//   const balance = +prompt("Add your balance");
 
-const previous =  localStorage.getItem('users')
-const users = previous ? JSON.parse(previous): []
+//   if (!!username && !isNaN(balance) && balance > 0) {
+//     const newUser = {
+//       username: username,
+//       id: crypto.randomUUID(),
+//       account: crypto.randomUUID(),
+//       balance: parseFloat(balance),
+//     };
+//     users.push(newUser);
+//     localStorage.setItem("users", JSON.stringify(users));
+//     displayUsers();
+//   } else {
+//     alert(
+//       `Failed to get Data: invalid data or account or balance is not number`
+//     );
+//   }
+// }
 
-function adduser() {
-  const username = prompt("Add your username");
-  const account = prompt("Add your accout number");
-  const balance = prompt("Add your balance");
-  if (
-    (username !== null && username.trim() !== "",
-    account !== null && account.trim() !== "" && account !== users.account,
-    balance !== null && balance.trim() !== "" && balance >= 0)
-  ) {
-    return {
+// const getCardItem = (name, account, balance, id) => {
+//   const template = `
+//       <div class="user">
+//       <h3>Username: ${name}</h3>
+//       <p>Account: ${account}</p>
+//       <p>Balance: ${balance}</p>
+//     <div class="btns">
+//     <button class="button" id="delete" onclick="removeUser('${id}')">Delete</button>
+//     <button class="button" id="edit" onclick="editUser(${id})">Edit</button>
+//     <button class="button" id="deposit" onclick="deposit(${id})">Deposit</button>
+//     <button class="button" id="withdraw" onclick="withdraw(${id})">Withdraw</button>
+//     </div>
+//       </div>
+//  `;
+//   return template;
+// };
+
+// function displayUsers() {
+//   const cardContainer = document.getElementById("data");
+//   const oldUsers = localStorage.getItem("users");
+//   // console.log(oldUsers);
+//   const storedUsers = oldUsers ? JSON.parse(oldUsers) : [];
+
+//   cardContainer.innerHTML = storedUsers
+//     .map(({ username: name, balance, id, account }) => {
+//       return getCardItem(name, account, balance, id);
+//     })
+//     .join("");
+// }
+
+// function withdraw(userId) {
+//   const user = users.find((item) => item.id === userId);
+
+//   if (user) {
+//     const withdrawAmount = +prompt("Enter your withdraw amount");
+//     if (
+//       !isNaN(withdrawAmount) &&
+//       withdrawAmount > 0 &&
+//       user.balance >= withdrawAmount
+//     ) {
+//       user.balance -= withdrawAmount;
+//       localStorage.setItem("users", JSON.stringify(users));
+//       alert(`Withdraw ${withdrawAmount} successful`);
+//       displayUsers();
+//     } else {
+//       alert(`Withdraw failed: insufficient balance or invalid amount`);
+//     }
+//   }
+// }
+
+// function deposit(userId) {
+//   const user = users.find((user) => user.id === userId);
+
+//   if (user) {
+//     const depositAmount = parseFloat(prompt("Enter your deposit amount"));
+//     if (!isNaN(depositAmount) && depositAmount > 0) {
+//       user.balance += depositAmount;
+//       localStorage.setItem("users", JSON.stringify(users));
+//       displayUsers();
+//     } else {
+//       alert(`Your deposit amount is ${depositAmount} invalid`);
+//     }
+//   }
+// }
+
+// function editUser(userId) {
+//   console.log(userId);
+//   const user = users.find((user) => user.id === userId);
+
+//   if (user) {
+//     const updatedName = prompt("Enter new username:");
+
+//     if (updatedName !== null && updatedName.trim() !== "") {
+//       user.username = updatedName;
+//       localStorage.setItem("users", JSON.stringify(users));
+//       displayUsers();
+//     }
+//   }
+// }
+
+// function removeUser(userId) {
+//   const result = users.filter((user) => user.id !== userId);
+//   localStorage.setItem("users", JSON.stringify(result));
+
+//   displayUsers();
+// }
+
+// const addbtn = document.getElementById("add");
+// addbtn.onclick = function () {
+//   adduser();
+//   displayUsers();
+// };
+
+// displayUsers();
+
+const getUpdatedUsers = () => {
+  const oldUsers = localStorage.getItem("users");
+  const storedUsers = oldUsers ? JSON.parse(oldUsers) : [];
+  return storedUsers;
+};
+
+function addUser() {
+  const username = prompt("Add your username")?.trim();
+  const balance = +prompt("Add your balance");
+
+  const storedUsers = getUpdatedUsers();
+
+  if (!!username && !isNaN(balance) && balance > 0) {
+    const newUser = {
       username: username,
-      id: users.length ? users[users.length - 1].id + 1 : 1,
-      account: account,
+      id: crypto.randomUUID(),
+      account: crypto.randomUUID(),
       balance: parseFloat(balance),
     };
+    storedUsers.push(newUser);
+    localStorage.setItem("users", JSON.stringify(storedUsers));
+    displayUsers();
   } else {
     alert(
       `Failed to get Data: invalid data or account or balance is not number`
@@ -50,72 +146,43 @@ function adduser() {
   }
 }
 
-function add(user) {
-  users.push(user);
-  localStorage.setItem("users", JSON.stringify(users));
-}
-
-const addbtn = document.getElementById("add");
-addbtn.onclick = function () {
-  const newUser = adduser();
-  add(newUser);
-  displayUsers();
-};
-
 function displayUsers() {
-  //   const user = document.getElementById("data");
-  //  const allusers = localStorage.getItem('users')
-  //  const users = JSON.parse(allusers)
-  // user.innerHTML = users.
-  const user = document.getElementById("data");
-  const storedUsers = JSON.parse(localStorage.getItem("users"))||[]
- 
-  user.innerHTML = storedUsers
-    .map((user) => {
-      return `
-      <div class="user">
-      <h3>Username: ${user.username}</h3>
-      <p>Account: ${user.account}</p>
-      <p>Balance: ${user.balance}</p>
-    <div class="btns">
-    <button class="button" id="delete" onclick="removeUser(${user.id})">Delete</button>
-    <button class="button" id="edit" onclick="editUser(${user.id})">Edit</button>
-    <button class="button" id="deposit" onclick="deposit(${user.id})">Deposit</button>
-    <button class="button" id="withdraw" onclick="withdraw(${user.id})">Withdraw</button>
-    </div>
-      </div>
- `;
-    })
+  const cardContainer = document.getElementById("cards-container");
+
+  const storedUsers = getUpdatedUsers();
+  cardContainer.innerHTML = storedUsers
+    .map((user) => getCardItem(user))
     .join("");
 }
-displayUsers();
 
 function withdraw(userId) {
+  const users = getUpdatedUsers();
   const user = users.find((item) => item.id === userId);
 
   if (user) {
-    const withdrawAmount = parseFloat(prompt("Enter your withdraw amount"));
+    const withdrawAmount = +prompt("Enter your withdraw amount");
     if (
-      withdrawAmount !== null &&
-      withdrawAmount >= 0 &&
+      !isNaN(withdrawAmount) &&
+      withdrawAmount > 0 &&
       user.balance >= withdrawAmount
     ) {
       user.balance -= withdrawAmount;
       localStorage.setItem("users", JSON.stringify(users));
       alert(`Withdraw ${withdrawAmount} successful`);
+      displayUsers();
     } else {
       alert(`Withdraw failed: insufficient balance or invalid amount`);
     }
   }
-  displayUsers();
 }
 
 function deposit(userId) {
+  const users = getUpdatedUsers();
   const user = users.find((user) => user.id === userId);
 
   if (user) {
     const depositAmount = parseFloat(prompt("Enter your deposit amount"));
-    if (depositAmount !== null && depositAmount >= 0) {
+    if (!isNaN(depositAmount) && depositAmount > 0) {
       user.balance += depositAmount;
       localStorage.setItem("users", JSON.stringify(users));
       displayUsers();
@@ -126,6 +193,7 @@ function deposit(userId) {
 }
 
 function editUser(userId) {
+  const users = getUpdatedUsers();
   const user = users.find((user) => user.id === userId);
 
   if (user) {
@@ -133,17 +201,40 @@ function editUser(userId) {
 
     if (updatedName !== null && updatedName.trim() !== "") {
       user.username = updatedName;
-localStorage.setItem("users", JSON.stringify(users));
+      localStorage.setItem("users", JSON.stringify(users));
       displayUsers();
     }
   }
 }
 
 function removeUser(userId) {
-  const i = users.findIndex((user) => user.id === userId);
-  if (i !== -1) {
-    users.splice(i, 1);
-    localStorage.setItem("users", JSON.stringify(users));
-  }
+  const users = getUpdatedUsers();
+  const result = users.filter((user) => user.id !== userId);
+
+  localStorage.setItem("users", JSON.stringify(result));
+
   displayUsers();
 }
+
+function getCardItem(user) {
+  const { username: name, balance, id, account } = user;
+  return `
+      <div class="user">
+      <h3>Username: ${name}</h3>
+      <p>Account: ${account}</p>
+      <p>Balance: ${balance}</p>
+    <div class="btns">
+    <button class="button" id="delete" onclick="removeUser('${id}')">Delete</button>
+    <button class="button" id="edit" onclick="editUser('${id}')">Edit</button>
+    <button class="button" id="deposit" onclick="deposit('${id}')">Deposit</button>
+    <button class="button" id="withdraw" onclick="withdraw('${id}')">Withdraw</button>
+    </div>
+      </div>
+ `;
+}
+
+const addBtn = document.getElementById("add");
+
+addBtn.onclick = addUser;
+
+displayUsers();
